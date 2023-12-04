6d644ff85c509daf7b997e872e4ad7b7da04e5cb
elizielx <yehezkieldio35@gmail.com>
2023-12-04 17:14:40 +0800

    feat: add types

--- src/connect.ts
+++ src/connect.ts
@@ -1,11 +1,12 @@
 import { ConnectRouter } from "@connectrpc/connect";
 import { ElizaService } from "./gen/eliza_connect";
+import { SayRequest, SayResponse } from "./gen/eliza_pb";
 
 export default (router: ConnectRouter) =>
-  router.service(ElizaService, {
-    async say(req) {
-      return {
-        sentence: `You said ${req.sentence}`,
-      };
-    },
-  });
+    router.service(ElizaService, {
+        async say(req: SayRequest) {
+            return new SayResponse({
+                sentence: `You said: ${req.sentence}`,
+            });
+        },
+    });
